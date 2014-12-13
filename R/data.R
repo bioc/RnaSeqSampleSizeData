925492d8ce419c2f8ae98805489544accad44953
Marc Carlson <mcarlson@fhcrc.org>
2014-12-13 02:01:17 +0000

    Adds RnaSeqSampleSizeData/ WES.1KG.WUGSC/  to the repos.
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/trunk/experiment/pkgs/RnaSeqSampleSizeData@3152 db2202da-8704-0410-b924-b125b3cd84ef

---
+++ R/data.R
@@ -0,0 +1,106 @@
+#' @import edgeR
+#' @exportPattern "."
+#' @name TCGA_BLCA
+#' @title Read counts and dispersion distribution for genes in TCGA BLCA data.
+#' @source \url{https://tcga-data.nci.nih.gov/tcgafiles/ftp_auth/distro_ftpusers/anonymous/tumor/blca/cgcc/unc.edu/illuminahiseq_rnaseqv2/rnaseqv2/unc.edu_BLCA.IlluminaHiSeq_RNASeqV2.Level_3.1.17.0/}
+#' @return A class 'DGEList' from edgeR package.
+#' @description Read counts and dispersion distribution for genes in TCGA BLCA data.
+#' @docType data
+#' @usage TCGA_BLCA
+NULL
+#' @name TCGA_BRCA
+#' @title Read counts and dispersion distribution for genes in TCGA BRCA data.
+#' @source \url{https://tcga-data.nci.nih.gov/tcgafiles/ftp_auth/distro_ftpusers/anonymous/tumor/brca/cgcc/unc.edu/illuminahiseq_rnaseqv2/rnaseqv2/unc.edu_BRCA.IlluminaHiSeq_RNASeqV2.Level_3.1.10.0/}
+#' @return A class 'DGEList' from edgeR package.
+#' @description Read counts and dispersion distribution for genes in TCGA BRCA data.
+#' @docType data
+#' @usage TCGA_BRCA
+NULL
+#' @name TCGA_CESC
+#' @title Read counts and dispersion distribution for genes in TCGA CESC data.
+#' @source \url{https://tcga-data.nci.nih.gov/tcgafiles/ftp_auth/distro_ftpusers/anonymous/tumor/cesc/cgcc/unc.edu/illuminahiseq_rnaseqv2/rnaseqv2/}
+#' @return A class 'DGEList' from edgeR package.
+#' @description Read counts and dispersion distribution for genes in TCGA CESC data.
+#' @docType data
+#' @usage TCGA_CESC
+NULL
+#' @name TCGA_COAD
+#' @title Read counts and dispersion distribution for genes in TCGA COAD data.
+#' @source \url{https://tcga-data.nci.nih.gov/tcgafiles/ftp_auth/distro_ftpusers/anonymous/tumor/coad/cgcc/unc.edu/illuminahiseq_rnaseqv2/rnaseqv2/unc.edu_COAD.IlluminaHiSeq_RNASeqV2.Level_3.1.9.0/}
+#' @return A class 'DGEList' from edgeR package.
+#' @description Read counts and dispersion distribution for genes in TCGA COAD data.
+#' @docType data
+#' @usage TCGA_COAD
+NULL
+#' @name TCGA_HNSC
+#' @title Read counts and dispersion distribution for genes in TCGA HNSC data.
+#' @source \url{https://tcga-data.nci.nih.gov/tcgafiles/ftp_auth/distro_ftpusers/anonymous/tumor/hnsc/cgcc/unc.edu/illuminahiseq_rnaseqv2/rnaseqv2/unc.edu_HNSC.IlluminaHiSeq_RNASeqV2.Level_3.1.7.0/}
+#' @return A class 'DGEList' from edgeR package.
+#' @description Read counts and dispersion distribution for genes in TCGA HNSC data.
+#' @docType data
+#' @usage TCGA_HNSC
+NULL
+#' @name TCGA_KIRC
+#' @title Read counts and dispersion distribution for genes in TCGA KIRC data.
+#' @source \url{https://tcga-data.nci.nih.gov/tcgafiles/ftp_auth/distro_ftpusers/anonymous/tumor/kirc/cgcc/unc.edu/illuminahiseq_rnaseqv2/rnaseqv2/unc.edu_KIRC.IlluminaHiSeq_RNASeqV2.Level_3.1.5.0/}
+#' @return A class 'DGEList' from edgeR package.
+#' @description Read counts and dispersion distribution for genes in TCGA KIRC data.
+#' @docType data
+#' @usage TCGA_KIRC
+NULL
+#' @name TCGA_LGG
+#' @title Read counts and dispersion distribution for genes in TCGA LGG data.
+#' @source \url{https://tcga-data.nci.nih.gov/tcgafiles/ftp_auth/distro_ftpusers/anonymous/tumor/lgg/cgcc/unc.edu/illuminahiseq_rnaseqv2/rnaseqv2/unc.edu_LGG.IlluminaHiSeq_RNASeqV2.Level_3.1.12.0/}
+#' @return A class 'DGEList' from edgeR package.
+#' @description Read counts and dispersion distribution for genes in TCGA LGG data.
+#' @docType data
+#' @usage TCGA_LGG
+NULL
+#' @name TCGA_LUAD
+#' @title Read counts and dispersion distribution for genes in TCGA LUAD data.
+#' @source \url{https://tcga-data.nci.nih.gov/tcgafiles/ftp_auth/distro_ftpusers/anonymous/tumor/luad/cgcc/unc.edu/illuminahiseq_rnaseqv2/rnaseqv2/unc.edu_LUAD.IlluminaHiSeq_RNASeqV2.Level_3.1.13.0/}
+#' @return A class 'DGEList' from edgeR package.
+#' @description Read counts and dispersion distribution for genes in TCGA LUAD data.
+#' @docType data
+#' @usage TCGA_LUAD
+NULL
+#' @name TCGA_LUSC
+#' @title Read counts and dispersion distribution for genes in TCGA LUSC data.
+#' @source \url{https://tcga-data.nci.nih.gov/tcgafiles/ftp_auth/distro_ftpusers/anonymous/tumor/lusc/cgcc/unc.edu/illuminahiseq_rnaseqv2/rnaseqv2/unc.edu_LUSC.IlluminaHiSeq_RNASeqV2.Level_3.1.9.0/}
+#' @return A class 'DGEList' from edgeR package.
+#' @description Read counts and dispersion distribution for genes in TCGA LUSC data.
+#' @docType data
+#' @usage TCGA_LUSC
+NULL
+#' @name TCGA_PRAD
+#' @title Read counts and dispersion distribution for genes in TCGA PRAD data.
+#' @source \url{https://tcga-data.nci.nih.gov/tcgafiles/ftp_auth/distro_ftpusers/anonymous/tumor/prad/cgcc/unc.edu/illuminahiseq_rnaseqv2/rnaseqv2/unc.edu_PRAD.IlluminaHiSeq_RNASeqV2.Level_3.1.12.0/}
+#' @return A class 'DGEList' from edgeR package.
+#' @description Read counts and dispersion distribution for genes in TCGA PRAD data.
+#' @docType data
+#' @usage TCGA_PRAD
+NULL
+#' @name TCGA_READ
+#' @title Read counts and dispersion distribution for genes in TCGA READ data.
+#' @source \url{https://tcga-data.nci.nih.gov/tcgafiles/ftp_auth/distro_ftpusers/anonymous/tumor/read/cgcc/unc.edu/illuminahiseq_rnaseqv2/rnaseqv2/unc.edu_READ.IlluminaHiSeq_RNASeqV2.Level_3.1.8.0/}
+#' @return A class 'DGEList' from edgeR package.
+#' @description Read counts and dispersion distribution for genes in TCGA READ data.
+#' @docType data
+#' @usage TCGA_READ
+NULL
+#' @name TCGA_THCA
+#' @title Read counts and dispersion distribution for genes in TCGA THCA data.
+#' @source \url{https://tcga-data.nci.nih.gov/tcgafiles/ftp_auth/distro_ftpusers/anonymous/tumor/thca/cgcc/unc.edu/illuminahiseq_rnaseqv2/rnaseqv2/unc.edu_THCA.IlluminaHiSeq_RNASeqV2.Level_3.1.12.0/}
+#' @return A class 'DGEList' from edgeR package.
+#' @description Read counts and dispersion distribution for genes in TCGA THCA data.
+#' @docType data
+#' @usage TCGA_THCA
+NULL
+#' @name TCGA_UCEC
+#' @title Read counts and dispersion distribution for genes in TCGA UCEC data.
+#' @source \url{https://tcga-data.nci.nih.gov/tcgafiles/ftp_auth/distro_ftpusers/anonymous/tumor/ucec/cgcc/unc.edu/illuminahiseq_rnaseqv2/rnaseqv2/unc.edu_UCEC.IlluminaHiSeq_RNASeqV2.Level_3.1.10.0/}
+#' @return A class 'DGEList' from edgeR package.
+#' @description Read counts and dispersion distribution for genes in TCGA UCEC data.
+#' @docType data
+#' @usage TCGA_UCEC
+NULL
